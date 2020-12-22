846bc08dd6ea67ec3327beec0122871fc5dd6ab1
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-12-22 12:35:12 +0100

    Update Actor.cs

--- ravenfield/Actor.cs
+++ ravenfield/Actor.cs
@@ -1,23 +1,18 @@
-public class Actor : Hurtable
-{
+public class Actor: Hurtable {
 
-	public virtual void Awake()
-	{
-		if (!this.aiControlled)
-		{
-			this.maxHealth = 20000f;
-			this.health = 20000f;
-		}
-	}
-  private void UpdateWeapon()
-  {
-  	if (!this.aiControlled)
-		{
-			this.activeWeapon.heat = 0f;
-			this.activeWeapon.ammo = 999;
-			this.activeWeapon.projectileSpeed = this.activeWeapon.projectileSpeed * 100f;
-			this.activeWeapon.configuration.kickback = 0f;
-			this.activeWeapon.configuration.cooldown = 0f;
-		}
-   }
+  public virtual void Awake() {
+    if (!this.aiControlled) {
+      this.maxHealth = 20000f;
+      this.health = 20000f;
+    }
+  }
+  private void UpdateWeapon() {
+    if (!this.aiControlled) {
+      this.activeWeapon.heat = 0f;
+      this.activeWeapon.ammo = 999;
+      this.activeWeapon.projectileSpeed = this.activeWeapon.projectileSpeed * 100f;
+      this.activeWeapon.configuration.kickback = 0f;
+      this.activeWeapon.configuration.cooldown = 0f;
+    }
+  }
 }
