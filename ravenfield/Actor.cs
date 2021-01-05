b3a419a867c2638fa7d318c463128cf9f42bd9bc
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-01-05 13:53:21 +0100

    Update Actor.cs

--- ravenfield/Actor.cs
+++ ravenfield/Actor.cs
@@ -1,18 +1,19 @@
 public class Actor: Hurtable {
 
   public virtual void Awake() {
-    if (!this.aiControlled) {
-      this.maxHealth = 20000f;
-      this.health = 20000f;
+    if (!this.aiControlled) {//Not An AI
+      this.maxHealth = 20000f;//Max Helth every respawn
+      this.health = 20000f;//Helth every respawn
     }
   }
   private void UpdateWeapon() {
-    if (!this.aiControlled) {
-      this.activeWeapon.heat = 0f;
-      this.activeWeapon.ammo = 999;
-      this.activeWeapon.projectileSpeed = this.activeWeapon.projectileSpeed * 100f;
-      this.activeWeapon.configuration.kickback = 0f;
-      this.activeWeapon.configuration.cooldown = 0f;
+    
+    if (!this.aiControlled) {//If its not an AI
+      this.activeWeapon.heat = 0f;//No weapon cooldown
+      this.activeWeapon.ammo = 999;//Literraly infinity because its do everytime
+      this.activeWeapon.projectileSpeed = this.activeWeapon.projectileSpeed * 2f;//range ?
+      this.activeWeapon.configuration.kickback = 0f;//Recoil
+      this.activeWeapon.configuration.cooldown = 0f;//No Cooldown
     }
   }
 }
