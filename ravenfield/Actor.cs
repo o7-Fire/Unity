be7a591a656d560f1eb3de710389a23f0e3c12f2
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-01-05 14:14:34 +0100

    Update Actor.cs

--- ravenfield/Actor.cs
+++ ravenfield/Actor.cs
@@ -11,7 +11,7 @@ public class Actor: Hurtable {
     if (!this.aiControlled) {//If its not an AI
       this.activeWeapon.heat = 0f;//No weapon cooldown
       this.activeWeapon.ammo = 999;//Literraly infinity because its do everytime
-      this.activeWeapon.projectileSpeed = this.activeWeapon.projectileSpeed * 2f;//range ?
+      this.activeWeapon.projectileSpeed = this.activeWeapon.projectileSpeed * 2f;//speed also increase range
       this.activeWeapon.configuration.kickback = 0f;//Recoil
       this.activeWeapon.configuration.cooldown = 0f;//No Cooldown
     }
