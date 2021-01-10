5923450b0e3077bcc0817537d4b7f90631b10a5f
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-01-10 02:53:14 +0100

    Update Actor.cs

--- ravenfield/Actor.cs
+++ ravenfield/Actor.cs
@@ -1,19 +1,31 @@
 public class Actor: Hurtable {
 
   public virtual void Awake() {
-    if (!this.aiControlled) {//Not An AI
-      this.maxHealth = 20000f;//Max Helth every respawn
-      this.health = 20000f;//Helth every respawn
+    //Code...
+    if (!this.aiControlled) { //Not An AI
+      this.maxHealth = 20000 f; //Max Helth every respawn
+      this.health = 20000 f; //Helth every respawn
     }
   }
-  private void UpdateWeapon() {
-    
-    if (!this.aiControlled) {//If its not an AI
-      this.activeWeapon.heat = 0f;//No weapon cooldown
-      this.activeWeapon.ammo = 999;//Literraly infinity because its do everytime
-      this.activeWeapon.projectileSpeed = this.activeWeapon.projectileSpeed * 2f;//speed also increase range
-      this.activeWeapon.configuration.kickback = 0f;//Recoil
-      this.activeWeapon.configuration.cooldown = 0f;//No Cooldown
+  public virtual void Update() {
+    if (this.IsSeated()) {//Make sure inside a vehicle
+      Vehicle vehicle = this.seat.vehicle;
+      if (vehicle != null) {
+        vehicle.maxHealth = 1200 f;
+        vehicle.health = 1000 f;//unlimited helth
+      }
     }
+    //Code... 
+  }
+  private void UpdateWeapon() {
+
+    if (!this.aiControlled) { //If its not an AI
+      this.activeWeapon.heat = 0 f; //No weapon cooldown
+      this.activeWeapon.ammo = 999; //Literraly infinity because its do everytime
+      this.activeWeapon.projectileSpeed = this.activeWeapon.projectileSpeed * 2 f; //speed also increase range
+      this.activeWeapon.configuration.kickback = 0 f; //Recoil
+      this.activeWeapon.configuration.cooldown = 0 f; //No Cooldown
+    }
+    //Code...
   }
 }
