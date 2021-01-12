00de8743ee0e1270b9117341f2001cb1491c047d
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-01-12 06:41:32 +0100

    Update Actor.cs

--- ravenfield/Actor.cs
+++ ravenfield/Actor.cs
@@ -7,14 +7,24 @@ public class Actor: Hurtable {
       this.health = 20000 f; //Helth every respawn
     }
   }
+  public void SwitchWeapon(int slot) {
+    //Code....
+    if (this.aiControlled) {
+      return;
+    }
+    this.activeWeapon.configuration.kickback = 0 f; //Recoil
+    this.activeWeapon.configuration.cooldown = 0 f; //No Cooldown
+    this.activeWeapon.configuration.projectilesPerShot = this.activeWeapon.configuration.projectilesPerShot * (this.activeWeapon.configuration.projectilesPerShot + 1); //shotgun = 2X shotgun
+    this.activeWeapon.projectileSpeed = this.activeWeapon.projectileSpeed * 2 f; //speed also increase range
+  }
   public virtual void Update() {
-    if (!this.aiControlled && this.IsSeated()) {//Make sure inside a vehicle and not an AI
+    if (!this.aiControlled && this.IsSeated()) { //Make sure inside a vehicle and not an AI
       Vehicle vehicle = this.seat.vehicle;
       if (vehicle != null) {
-			vehicle.maxHealth = 20000f;//Max
-			vehicle.health = 15000f;//Current
-			vehicle.canBeRepairedAfterDeath = true;// revive
-			vehicle.Repair(10000f);//auto repair
+        vehicle.maxHealth = 20000 f; //Max
+        vehicle.health = 15000 f; //Current
+        vehicle.canBeRepairedAfterDeath = true; // revive
+        vehicle.Repair(10000 f); //auto repair
       }
     }
     //Code... 
@@ -24,10 +34,6 @@ public class Actor: Hurtable {
     if (!this.aiControlled) { //If its not an AI
       this.activeWeapon.heat = 0 f; //No weapon cooldown
       this.activeWeapon.ammo = 9999; //Literraly infinity because its do everytime
-      this.activeWeapon.projectileSpeed = this.activeWeapon.projectileSpeed * 2 f; //speed also increase range
-      this.activeWeapon.configuration.kickback = 0 f; //Recoil
-      this.activeWeapon.configuration.cooldown = 0 f; //No Cooldown
-	this.activeWeapon.configuration.projectilesPerShot = this.activeWeapon.configuration.projectilesPerShot * ( this.activeWeapon.configuration.projectilesPerShot + 1);//shotgun = 2X shotgun
     }
     //Code...
   }
